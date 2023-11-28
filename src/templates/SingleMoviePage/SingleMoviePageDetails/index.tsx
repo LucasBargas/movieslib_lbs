499ce15666538103113345b056938f9612f6d835
Lucas Bargas <lucas.bargasoficial@gmail.com>
2023-11-27 23:21:07 -0300

    Single movie page - developed

--- src/templates/SingleMoviePage/SingleMoviePageDetails/index.tsx
+++ src/templates/SingleMoviePage/SingleMoviePageDetails/index.tsx
@@ -24,94 +24,108 @@ export const SingleMoviePageDetails = async ({
 
   return (
     <S.SingleMoviePageDetailsContainer>
-      <S.SingleMoviePageDetailsTitle>
-        <span>
-          <i>
-            <FaStar />
-          </i>
-          {Number(movie.vote_average).toFixed(1)}
-        </span>
+      {!!movie.vote_average && !!movie.title && (
+        <S.SingleMoviePageDetailsTitle>
+          <span>
+            <i>
+              <FaStar />
+            </i>
+            {Number(movie.vote_average).toFixed(1)}
+          </span>
 
-        <h1>{movie.title}</h1>
-      </S.SingleMoviePageDetailsTitle>
+          <h1>{movie.title}</h1>
+        </S.SingleMoviePageDetailsTitle>
+      )}
 
-      <S.SingleMoviePageDetailsOriginalTitle>
-        <strong>Título original:</strong> {movie.original_title}
-      </S.SingleMoviePageDetailsOriginalTitle>
+      {!!movie.original_title && (
+        <S.SingleMoviePageDetailsOriginalTitle>
+          <strong>Título original:</strong> {movie.original_title}
+        </S.SingleMoviePageDetailsOriginalTitle>
+      )}
 
-      <S.SingleMoviePageDetailsOriginalGenres>
-        <ul>
-          {movie.genres!.map((genre) => (
-            <li key={genre.id}>
-              <Link
-                href={`/filme?genero_id=${genre.id}&pagina=1
+      {!!movie.genres && (
+        <S.SingleMoviePageDetailsOriginalGenres>
+          <ul>
+            {movie.genres.map((genre) => (
+              <li key={genre.id}>
+                <Link
+                  href={`/filme?genero_id=${genre.id}&pagina=1
               `}
-              >
-                {genre.name}
-              </Link>
+                >
+                  {genre.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </S.SingleMoviePageDetailsOriginalGenres>
+      )}
+
+      {!!movie.tagline && !!movie.overview && (
+        <S.SingleMoviePageDetailsMoreInfos>
+          <ul>
+            <li>
+              <h2>
+                <i>
+                  <BsFileEarmarkText />
+                </i>
+                Sinopse:
+              </h2>
             </li>
-          ))}
-        </ul>
-      </S.SingleMoviePageDetailsOriginalGenres>
+            <li>
+              <p>{movie.tagline}</p>
+              <p>{movie.overview}</p>
+            </li>
+          </ul>
+        </S.SingleMoviePageDetailsMoreInfos>
+      )}
 
-      <S.SingleMoviePageDetailsMoreInfos>
-        <ul>
-          <li>
-            <h2>
-              <i>
-                <BsFileEarmarkText />
-              </i>
-              Sinopse:
-            </h2>
-          </li>
-          <li>
-            <p>{movie.tagline}</p>
-            <p>{movie.overview}</p>
-          </li>
-        </ul>
-      </S.SingleMoviePageDetailsMoreInfos>
+      {!!movie.runtime && (
+        <S.SingleMoviePageDetailsMoreInfos>
+          <ul>
+            <li>
+              <h2>
+                <i>
+                  <WiTime3 />
+                </i>
+                Duração:
+              </h2>
+            </li>
+            <li>{movie.runtime} minutos</li>
+          </ul>
+        </S.SingleMoviePageDetailsMoreInfos>
+      )}
 
-      <S.SingleMoviePageDetailsMoreInfos>
-        <ul>
-          <li>
-            <h2>
-              <i>
-                <WiTime3 />
-              </i>
-              Duração:
-            </h2>
-          </li>
-          <li>{movie.runtime} minutos</li>
-        </ul>
-      </S.SingleMoviePageDetailsMoreInfos>
+      {!!movie.budget && (
+        <S.SingleMoviePageDetailsMoreInfos>
+          <ul>
+            <li>
+              <h2>
+                <i>
+                  <IoWalletOutline />
+                </i>
+                Orçamento:
+              </h2>
+            </li>
+            <li>{handleConvertPrice(movie.budget)}</li>
+          </ul>
+        </S.SingleMoviePageDetailsMoreInfos>
+      )}
 
-      <S.SingleMoviePageDetailsMoreInfos>
-        <ul>
-          <li>
-            <h2>
-              <i>
-                <IoWalletOutline />
-              </i>
-              Orçamento:
-            </h2>
-          </li>
-          <li>{handleConvertPrice(movie.budget!)}</li>
-        </ul>
-      </S.SingleMoviePageDetailsMoreInfos>
-
-      <S.SingleMoviePageDetailsMoreInfos>
-        <ul>
-          <li>
-            <h2>
-              <i>
-                <BsGraphUp />
-              </i>
-              Receita:
-            </h2>
-          </li>
-          <li>{handleConvertPrice(movie.revenue!)}</li>
-        </ul>
-      </S.SingleMoviePageDetailsMoreInfos>
+      {!!movie.revenue && (
+        <S.SingleMoviePageDetailsMoreInfos>
+          <ul>
+            <li>
+              <h2>
+                <i>
+                  <BsGraphUp />
+                </i>
+                Receita:
+              </h2>
+            </li>
+            <li>{handleConvertPrice(movie.revenue)}</li>
+          </ul>
+        </S.SingleMoviePageDetailsMoreInfos>
+      )}
     </S.SingleMoviePageDetailsContainer>
   );
 };
