659ac2fb7ee7c66c9b2e9c3c36646e972daf749d
Lucas Bargas <lucas.bargasoficial@gmail.com>
2023-11-29 11:08:30 -0300

    Footer - fixed

--- src/layouts/AppWrapper/styles.ts
+++ src/layouts/AppWrapper/styles.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
 export const AppWrapperContainer = styled.section`
+  display: flex;
+  flex-direction: column;
   min-height: calc(100vh + 2rem);
+
+  main {
+    flex: 1;
+  }
 `;
